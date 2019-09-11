d956bf1cf2f4fb228b4db7bb2fa8af5654a736cd
ciherrera20 <ciherrera20@chaminet.org>
2019-09-10 17:57:25 -0700

    Cleared files

--- src/libIterativeRobot/commands/Autonomous/AutonGroup2.cpp
+++
@@ -1,6 +0,0 @@
-#include "libIterativeRobot/commands/Autonomous/AutonGroup2.h"
-#include "libIterativeRobot/commands/Miscellaneous/Delay.h"
-
-AutonGroup2::AutonGroup2() {
-  addSequentialCommand(new Delay(5000, 0));
-}
